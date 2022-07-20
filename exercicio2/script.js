49ef6720cc0f0ea55fe4bef81d28641e298e055f
Lucas Caffeu <lucas_caffeu_psico@hotmail.com>
2022-07-19 22:08:32 -0300

    feito-os-3-exercicios

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+// let pergunta1 = prompt('você é feliz?')
+// let pergunta2 = prompt('você é um robô?')
+// let pergunta3 = prompt('voce acordou cedo hoje?')
+
+// console.log(pergunta1,resposta1)
+// console.log(pergunta2,resposta2)
+// console.log(pergunta3,resposta3)
+
+const pergunta1 = "você é feliz?"
+const resposta1 = prompt(pergunta1)
+const pergunta2 = "você é um robô?"
+const resposta2 = prompt(pergunta2)
+const pergunta3 = "voce acordou cedo hoje?"
+const resposta3 = prompt(pergunta3)
+
+console.log(pergunta1,resposta1)
+console.log(pergunta2,resposta2)
+console.log(pergunta3,resposta3)
---
+++ exercicio3/script.js
@@ -0,0 +1,9 @@
+let A = 10
+let B = 25
+let C = A + B
+
+A = C - A
+B = C - B
+
+console.log("o novo valor de A é",A)
+console.log("o novo valor de B é",B)
